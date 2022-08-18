7c0a28c4bcad1fbb896008af78b7634a886a378e
JoaoZenaro <47560029+JoaoZenaro@users.noreply.github.com>
2022-08-17 21:31:43 -0300

    calculos funcoes

--- Calculos.cpp
+++ Calculos.cpp
@@ -15,6 +15,58 @@ void soma()
 
     soma = a + b;
 
-    printf("A soma é %d\n", soma);
+    printf("A soma ï¿½ %d\n", soma);
     
+}
+
+void subtracao()
+{
+    int a = 0;
+    int b = 0;
+    int sub = 0;
+
+
+    printf("Digite A \n");
+    scanf_s("%d", &a);
+    printf("Digite B: \n");
+    scanf_s("%d", &b);
+
+    sub = a - b;
+
+    printf("A subtracao eh %d\n", sub);
+}
+
+void multiplicacao()
+{
+    int a = 0;
+    int b = 0;
+    int res = 0;
+
+
+    printf("Digite A \n");
+    scanf_s("%d", &a);
+    printf("Digite B: \n");
+    scanf_s("%d", &b);
+
+    res = a * b;
+
+    printf("A multiplicacao eh %d\n", res);
+}
+
+
+void divisao()
+{
+    int a = 0;
+    int b = 0;
+    int res = 0;
+
+
+    printf("Digite A \n");
+    scanf_s("%d", &a);
+    printf("Digite B: \n");
+    scanf_s("%d", &b);
+
+    res = a / b;
+
+    printf("A divisao eh %d\n", res);
 }
